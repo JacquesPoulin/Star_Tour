aaf3f3eb0a9a5f4358391ca7c50e2238ff1fd8a7
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-07 18:03:41 +0200

    [STYLE] : font update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <h1 className="bg-blue-700">Contact</h1>
+      <h1 className="bg-blue-700 font-jedi">Contact</h1>
     </div>
   );
 };
