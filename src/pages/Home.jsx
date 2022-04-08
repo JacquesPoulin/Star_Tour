c480c7a2180f490734d34c538d6846ade567da16
Astrid-Froitier <asfroitier@gmail.com>
2022-04-08 20:10:09 +0200

    [features] i did the HomeLinks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import HomeLinks from "../components/home/HomeLinks";
 
 const Home = () => {
   return (
     <div>
       <h1 className="text-red-700 font-orb">Home</h1>
+      <HomeLinks />
     </div>
   );
 };
