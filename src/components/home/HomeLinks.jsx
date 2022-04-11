a6015f485d40bfd601ce5237fd8e74a20948bd87
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-11 07:06:07 +0200

    [STYLE] : i modified the navbar logo to adjust perfectly the navbar items

--- src/components/home/HomeLinks.jsx
+++ src/components/home/HomeLinks.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { NavLink } from "react";
 
 const HomeLinks = () => {
   return (
@@ -6,38 +6,39 @@ const HomeLinks = () => {
       <h1 className="text-4xl font-orb">Nos services</h1>
       <div className="grid w-3/4 h-full grid-cols-2 grid-rows-2 gap-8 mx-auto mt-12">
         <NavLink to="/Destinations">
-          <a
-            href="/src/pages/Booking"
+          <button
+            type="button"
             className="bg-[url('./assets/obione.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl shadow-2xl"
           >
             Découvrez toutes nos destinations !
-          </a>
+          </button>
         </NavLink>
         <NavLink to="/Reservation">
-          <a
-            href="./src/pages/Planets"
+          <button
+            type="button"
             className="bg-[url('./assets/obione1.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
           >
             Réservez votre prochain voyage !
-          </a>
+          </button>
         </NavLink>
 
         <NavLink to="/Vaisseaux">
-          <a
-            href="/src/pages/Ships"
+          <button
+            type="button"
             className="bg-[url('./assets/shiplink.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
           >
             Admirez notre flotte de vaisseaux !
-          </a>
+          </button>
         </NavLink>
 
         <NavLink to="/Contact">
-          <a
+          <button
+            type="button"
             className="bg-[url('./assets/storm.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-            href="/src/pages/Contact"
+            href="/Contact"
           >
             Contactez-nous !
-          </a>
+          </button>
         </NavLink>
       </div>
     </div>
