7d729f4323ba83f119b47b74ecaff00a3527b755
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-11 06:59:31 +0200

    [FEATURES: I added the links from the Homelinks component]

--- src/components/home/HomeLinks.jsx
+++ src/components/home/HomeLinks.jsx
@@ -5,30 +5,40 @@ const HomeLinks = () => {
     <div>
       <h1 className="text-4xl font-orb">Nos services</h1>
       <div className="grid w-3/4 h-full grid-cols-2 grid-rows-2 gap-8 mx-auto mt-12">
-        <a
-          href="/src/pages/Booking"
-          className="bg-[url('./assets/obione.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl shadow-2xl"
-        >
-          Découvrez toutes nos destinations !
-        </a>
-        <a
-          href="./src/pages/Planets"
-          className="bg-[url('./assets/obione1.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-        >
-          Réservez votre prochain voyage !
-        </a>
-        <a
-          href="/src/pages/Ships"
-          className="bg-[url('./assets/shiplink.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-        >
-          Admirez notre flotte de vaisseaux !
-        </a>
-        <a
-          className="bg-[url('./assets/storm.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-          href="/src/pages/Contact"
-        >
-          Contactez-nous !
-        </a>
+        <NavLink to="/Destinations">
+          <a
+            href="/src/pages/Booking"
+            className="bg-[url('./assets/obione.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl shadow-2xl"
+          >
+            Découvrez toutes nos destinations !
+          </a>
+        </NavLink>
+        <NavLink to="/Reservation">
+          <a
+            href="./src/pages/Planets"
+            className="bg-[url('./assets/obione1.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
+          >
+            Réservez votre prochain voyage !
+          </a>
+        </NavLink>
+
+        <NavLink to="/Vaisseaux">
+          <a
+            href="/src/pages/Ships"
+            className="bg-[url('./assets/shiplink.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
+          >
+            Admirez notre flotte de vaisseaux !
+          </a>
+        </NavLink>
+
+        <NavLink to="/Contact">
+          <a
+            className="bg-[url('./assets/storm.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
+            href="/src/pages/Contact"
+          >
+            Contactez-nous !
+          </a>
+        </NavLink>
       </div>
     </div>
   );
