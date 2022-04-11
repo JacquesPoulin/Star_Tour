eef1255d16bc49c84219563dee3b63af0bc9e472
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-11 07:09:04 +0200

    Revert "[STYLE] : i modified the navbar logo to adjust perfectly the navbar items"
    This reverts commit a6015f485d40bfd601ce5237fd8e74a20948bd87.

--- src/components/home/HomeLinks.jsx
+++ src/components/home/HomeLinks.jsx
@@ -1,4 +1,4 @@
-import React, { NavLink } from "react";
+import React from "react";
 
 const HomeLinks = () => {
   return (
@@ -6,39 +6,38 @@ const HomeLinks = () => {
       <h1 className="text-4xl font-orb">Nos services</h1>
       <div className="grid w-3/4 h-full grid-cols-2 grid-rows-2 gap-8 mx-auto mt-12">
         <NavLink to="/Destinations">
-          <button
-            type="button"
+          <a
+            href="/src/pages/Booking"
             className="bg-[url('./assets/obione.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl shadow-2xl"
           >
             Découvrez toutes nos destinations !
-          </button>
+          </a>
         </NavLink>
         <NavLink to="/Reservation">
-          <button
-            type="button"
+          <a
+            href="./src/pages/Planets"
             className="bg-[url('./assets/obione1.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
           >
             Réservez votre prochain voyage !
-          </button>
+          </a>
         </NavLink>
 
         <NavLink to="/Vaisseaux">
-          <button
-            type="button"
+          <a
+            href="/src/pages/Ships"
             className="bg-[url('./assets/shiplink.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
           >
             Admirez notre flotte de vaisseaux !
-          </button>
+          </a>
         </NavLink>
 
         <NavLink to="/Contact">
-          <button
-            type="button"
+          <a
             className="bg-[url('./assets/storm.png')] bg-cover bg-no-repeat h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-            href="/Contact"
+            href="/src/pages/Contact"
           >
             Contactez-nous !
-          </button>
+          </a>
         </NavLink>
       </div>
     </div>
