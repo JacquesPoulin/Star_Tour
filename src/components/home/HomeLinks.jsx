933708e354a648a86f0b6e30859c50b660a1d970
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-04-12 13:54:36 +0200

    [BUG FIX] fixed homepage

--- src/components/home/HomeLinks.jsx
+++ src/components/home/HomeLinks.jsx
@@ -1,45 +1,43 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const HomeLinks = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-orb">Nos services</h1>
-      <div className="grid w-3/4 h-full grid-cols-2 grid-rows-2 gap-8 mx-auto mt-12">
-        <NavLink to="/Destinations">
-          <button
-            type="button"
-            className="bg-[url('./assets/obione.png')] bg-cover bg-no-repeat w-96 h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl shadow-2xl"
-          >
-            Découvrez toutes nos destinations !
-          </button>
-        </NavLink>
-        <NavLink to="/Reservation">
-          <button
-            type="button"
-            className="bg-[url('./assets/obione1.png')] bg-cover bg-no-repeat w-96 h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-          >
+    <div className="pt-10 pb-28 bg-[url('./assets/images/dark_bg.jpg')] bg-cover bg-repeat">
+      <h1 className="pl-10 text-4xl font-orb text-slate-50">Nos services</h1>
+      <div className="grid w-3/4 h-full grid-cols-2 grid-rows-2 gap-10 mx-auto mt-12">
+        {/*Destinations*/}
+          <div className="overflow-hidden h-44 rounded-xl box-shadow-1">
+          <Link to="/Destinations">
+            <div className="bg-[url('./assets/images/obiwan.png')] bg-cover bg-no-repeat h-full bg-center flex justify-center items-center text-center text-white font-exo text-2xl md:text-3xl text-shadow-3 hover:scale-105 transition ease-in-out duration-500">
+              Découvrez toutes nos destinations !
+            </div>
+          </Link>
+          </div>
+        {/*Réservations*/}
+        <div className="overflow-hidden h-44 rounded-xl box-shadow-1">
+        <Link to="/Reservation">
+          <div className="bg-[url('./assets/images/obiwan2.png')] bg-cover bg-no-repeat h-full bg-center flex justify-center items-center text-center text-white font-exo text-2xl md:text-3xl text-shadow-3 hover:scale-105 transition ease-in-out duration-500">
             Réservez votre prochain voyage !
-          </button>
-        </NavLink>
-
-        <NavLink to="/Vaisseaux">
-          <button
-            type="button"
-            className="bg-[url('./assets/shiplink.png')] bg-cover bg-no-repeat w-96 h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-          >
+          </div>
+        </Link>
+        </div>
+        {/*Vaisseaux*/}
+        <div className="overflow-hidden h-44 rounded-xl box-shadow-1">
+        <Link to="/Vaisseaux">
+          <div className="bg-[url('./assets/images/shiplink.png')] bg-cover bg-no-repeat h-full bg-center flex justify-center items-center text-center text-white font-exo text-2xl md:text-3xl text-shadow-3 hover:scale-105 transition ease-in-out duration-500">
             Admirez notre flotte de vaisseaux !
-          </button>
-        </NavLink>
-
-        <NavLink to="/Contact">
-          <button
-            type="button"
-            className="bg-[url('./assets/storm.png')] bg-cover bg-no-repeat w-96 h-44 bg-center flex justify-center items-center text-center text-white font-exo text-2xl rounded-xl"
-          >
+          </div>
+        </Link>
+        </div>
+        {/*Contact*/}
+        <div className="overflow-hidden h-44 rounded-xl box-shadow-1">
+        <Link to="/Contact">
+          <div className="bg-[url('./assets/images/storm.png')] bg-cover bg-no-repeat h-full bg-center flex justify-center items-center text-center text-white font-exo text-2xl md:text-3xl text-shadow-3 hover:scale-105 transition ease-in-out duration-500">
             Contactez-nous !
-          </button>
-        </NavLink>
+          </div>
+        </Link>
+        </div>
       </div>
     </div>
   );
