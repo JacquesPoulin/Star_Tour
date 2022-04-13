75edb66c86620a3e8c36a5c93cfb9aa93aee349b
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-13 13:39:54 +0200

    [UPDATES] : PropTypes added to the PlanetCard modal

--- src/components/planets/PlanetCard.jsx
+++ src/components/planets/PlanetCard.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const PlanetCard = ({name, img}) => {
+const PlanetCard = ({ name, img }) => {
   return (
     <div className="flex items-center justify-center h-40 w-72">
-      <img src={img} className="absolute h-40 w-72 box-shadow-1 rounded-2xl" />
-      <p className="z-10 text-4xl text-slate-50 font-exo text-shadow-3">{name}</p>
+      <img
+        src={img}
+        className="absolute h-40 w-72 box-shadow-1 rounded-2xl"
+        alt={name}
+      />
+      <p className="z-10 text-4xl text-slate-50 font-exo text-shadow-3">
+        {name}
+      </p>
     </div>
   );
 };
