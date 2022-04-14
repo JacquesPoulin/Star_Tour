78ab5a9c61864121b50e53f43afeffb3fc2c12fc
Astrid-Froitier <asfroitier@gmail.com>
2022-04-14 18:05:42 +0200

    [STYLE] worked on filters planets and tailwind

---
+++ data/planetsFilters.js
@@ -0,0 +1,36 @@
+const planetsFilters = [
+  {
+    item: 1,
+    name: "Déserts",
+  },
+  {
+    item: 2,
+    name: "Océan",
+  },
+  {
+    item: 3,
+    name: "Plaines et Fôrets",
+  },
+  {
+    item: 4,
+    name: "Volcan",
+  },
+  {
+    item: 5,
+    name: "Neige",
+  },
+  {
+    item: 6,
+    name: "Marais",
+  },
+  {
+    item: 7,
+    name: "Astéroïde",
+  },
+  {
+    item: 8,
+    name: "Zone Urbaine",
+  },
+];
+
+export default planetsFilters;
--- src/components/planets/PlanetsFilter.jsx
+++ src/components/planets/PlanetsFilter.jsx
@@ -1,63 +1,20 @@
 import React from "react";
+import PlanetsTopography from "./PlanetsTopography";
+import planetsFilters from "../../../data/planetsFilters";
 
-const PlanetsFilter = () => {
+const PlanetsFilters = () => {
   return (
-    <div className="w-1/2 h-64 pt-20 pl-20">
-      <h1 className="mt-10 text-4xl text-left text-slate-50 font-orb text-shadow-wh">
+    <div className="h-64 pl-10 w-fit">
+      <h1 className="mt-10 text-4xl text-left text-slate-50 font-orb text-shadow-1">
         Filtrer les planètes :
       </h1>
-      <div className="grid grid-cols-4 grid-rows-2 gap-4 mt-4 ">
-        <button
-          className="items-center h-16 text-xl text-gray-800 rounded-md w-28 bg-amber-400 font-exo box-shadow-1 "
-          type="button"
-        >
-          Déserts
-        </button>
-        <button
-          className="items-center h-16 text-xl text-gray-800 rounded-md bg-sky-500 w-28 font-exo box-shadow-1 "
-          type="button"
-        >
-          Océans
-        </button>
-        <button
-          className="items-center h-16 text-xl text-gray-800 rounded-md bg-lime-600 w-28 font-exo box-shadow-1 "
-          type="button"
-        >
-          Plaines et Fôrets
-        </button>
-        <button
-          className="items-center h-16 text-xl text-gray-800 bg-red-600 rounded-md w-28 font-exo box-shadow-1 "
-          type="button"
-        >
-          Volcans
-        </button>
-        <button
-          className="items-center h-16 text-xl text-gray-800 rounded-md bg-zinc-300 w-28 font-exo box-shadow-1 "
-          type="button"
-        >
-          Neige
-        </button>
-        <button
-          className="items-center h-16 text-xl text-gray-800 rounded-md bg-lime-800 w-28 font-exo box-shadow-1 "
-          type="button"
-        >
-          Marais
-        </button>
-        <button
-          className="items-center h-16 text-xl text-gray-800 rounded-md bg-neutral-500 w-28 font-exo box-shadow-1 "
-          type="button"
-        >
-          Astéroïdes
-        </button>
-        <button
-          className="items-center h-16 text-xl text-gray-800 bg-teal-100 rounded-md w-28 font-exo box-shadow-1 "
-          type="button"
-        >
-          Zones Urbaines
-        </button>
+      <div className="grid grid-cols-4 grid-rows-2 gap-4 mt-4">
+        {planetsFilters.map((planetFilter) => (
+          <PlanetsTopography {...planetFilter} />
+        ))}
       </div>
     </div>
   );
 };
 
-export default PlanetsFilter;
+export default PlanetsFilters;
