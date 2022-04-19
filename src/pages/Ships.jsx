27230341e043f935849ef5bd616e1779c62f3a9d
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-04-19 18:54:58 +0200

    [FEATURE] created shipcard

--- src/pages/Ships.jsx
+++ src/pages/Ships.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import NavBar from "../components/Navbar";
+import ShipCard from "../components/ships/ShipCard";
 
 const Ships = () => {
   return (
-    <div>
+    <div className="bg-[url('./assets/images/dark_bg.jpg')] bg-[length:100vw] bg-repeat">
       <NavBar />
+      <div className="flex items-center justify-center pb-14 pt-44">
+        <h1 className="w-4/6 text-6xl text-center text-slate-50 font-orb text-shadow-wh">
+          Découvrez notre flotte de vaisseaux !
+        </h1>
+      </div>
+      <ShipCard />
+      <p>P</p>
     </div>
   );
 };
