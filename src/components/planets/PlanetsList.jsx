dcb13c399aa222d2b459062729109db6ee7d9068
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-20 18:16:08 +0200

    [BUG FIX] : I managed all the ESLint issues

--- src/components/planets/PlanetsList.jsx
+++ src/components/planets/PlanetsList.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import PlanetCard from "./PlanetCard";
 import planets from "../../../data/planets";
 
-const PlanetsList = ({search, filter}) => {
-
-  const [count, setCount] = useState(0);
+const PlanetsList = ({ search, filter }) => {
+  // const [count, setCount] = useState(0);
+  PlanetsList.propTypes = {
+    filter: PropTypes.string.isRequired,
+  };
+  PlanetsList.propTypes = {
+    search: PropTypes.string.isRequired,
+  };
 
   return (
     <div className="flex flex-wrap items-center justify-center gap-10 p-20">
       {planets
-      .filter((planet) => (planet.biome === filter) || !filter)
-      .filter((planet) => planet.name.toLowerCase().startsWith(search))
-      .map((planet) => (<PlanetCard {...planet} />))
-      }
+        .filter((planet) => planet.biome === filter || !filter)
+        .filter((planet) => planet.name.toLowerCase().startsWith(search))
+        .map((planet) => (
+          <PlanetCard {...planet} />
+        ))}
     </div>
   );
 };
