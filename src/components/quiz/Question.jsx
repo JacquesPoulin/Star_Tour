520f75a4371d4d586c347f692a380f2f6d0b304d
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-04-20 16:19:24 +0200

    [STYLE] : Responsive in progress

--- src/components/quiz/Question.jsx
+++ src/components/quiz/Question.jsx
@@ -28,38 +28,38 @@ const Question = () => {
       ) : (
         <>
           <div>
-            <div className="w-auto h-auto">
-              <div className="grid grid-cols-6 grid-rows-3 px-10 pt-36 place-content-center h-3/5 ">
-                <div className="col-span-2 row-span-4 text-center">
+            <div className="w-auto h-auto ">
+              <div className="grid grid-cols-6 grid-rows-3 px-10 pt-36 place-content-center h-3/5 bp2:grid bp2:grid-cols-3 bp2:grid-rows-2 bp2:px-10">
+                <div className="col-span-2 row-span-4 text-center bp2:grid bp2:col-span-1 bp2:row-span-2">
                   <img
-                    className="w-full h-full"
+                    className="w-full h-full "
                     src="assets/images/c3po.png"
                     alt="c3p0 et r2d2"
                   />
                 </div>
-                <div className="row-span-1 p-3 ml-16 text-5xl mt-14">
+                <div className="row-span-1 p-3 ml-16 text-3xl mt-14 bp2:row-span-1">
                   <span className="items-center justify-center p-10 text-center rounded-2xl font-exo text-shadow-3 text-slate-50 box-shadow-1 bg-black/50">
                     {currentQuestion + 1}/{quizForm.length}
                   </span>
                 </div>
-                <div className="col-span-3 row-span-1 text-5xl mt-14">
-                  <span className="items-center justify-center p-10 text-center text-slate-50 rounded-2xl font-exo text-shadow-3 box-shadow-1 bg-black/50">
+                <div className="col-span-3 row-span-1 p-3 text-3xl mt-14 bp2:col-span-1 bp2:row-span-1 bp2:text-xl">
+                  <span className="items-center justify-center p-10 text-center text-slate-50 rounded-2xl font-exo text-shadow-3 box-shadow-1 bg-black/50 ">
                     {quizForm[currentQuestion].questionText}
                   </span>
                 </div>
 
-                <div className="col-span-2 row-span-2 ">
+                <div className="col-span-2 row-span-2 bp2:col-span-1">
                   {quizForm[currentQuestion].answerOptions.map(
                     (answerOption) => (
                       <button
-                        className="flex items-center justify-center h-[25rem] w-[30rem] text-7xl text-center text-slate-50 font-exo text-shadow-3 "
+                        className="flex items-center justify-center h-[15rem] w-[20rem] text-4xl text-center text-slate-50 font-exo text-shadow-3 bp2:h-[7rem] bp2:w-[10rem] bp2:text-xl"
                         type="button"
                         onClick={() =>
                           handleAnswerOptionClick(answerOption.isCorrect)
                         }
                       >
                         <img
-                          className="absolute h-[25rem] w-[30rem] rounded-2xl box-shadow-1 hover:scale-105"
+                          className="absolute h-[15rem] w-[20rem] rounded-2xl box-shadow-1 hover:scale-105 bp2:h-[7rem] bp2:w-[10rem]"
                           src={answerOption.img}
                           alt={answerOption.answerText}
                         />
@@ -68,18 +68,18 @@ const Question = () => {
                     )
                   )}
                 </div>
-                <div className="col-span-2 row-span-2 ">
+                <div className="col-span-2 row-span-2 bp2:col-span-1">
                   {quizForm[currentQuestion].answerOptions2.map(
                     (answerOption2) => (
                       <button
-                        className="flex items-center justify-center  h-[25rem] w-[30rem]  text-7xl text-center text-slate-50 font-exo text-shadow-3 "
+                        className="flex items-center justify-center h-[15rem] w-[20rem] text-4xl text-center text-slate-50 font-exo text-shadow-3 bp2:h-[7rem] bp2:w-[10rem] bp2:text-xl "
                         type="button"
                         onClick={() =>
                           handleAnswerOptionClick(answerOption2.isCorrect)
                         }
                       >
                         <img
-                          className="absolute h-[25rem] w-[30rem]  rounded-2xl box-shadow-1 hover:scale-105"
+                          className="absolute h-[15rem] w-[20rem] rounded-2xl box-shadow-1 hover:scale-105 bp2:h-[7rem] bp2:w-[10rem] "
                           src={answerOption2.img2}
                           alt={answerOption2.answerText2}
                         />
