e5b5ec025fea90c3e105b6ee2b430157552f05b6
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-04-21 13:55:47 +0200

    [UPDATE]: page contact realized

--- src/components/ships/ShipCard.jsx
+++ src/components/ships/ShipCard.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line react/prop-types
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
