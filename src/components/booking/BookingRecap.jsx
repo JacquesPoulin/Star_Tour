be4638c01430fb844f0e47c180f8879d3bd55f6f
Astrid-Froitier <asfroitier@gmail.com>
2022-04-28 20:48:51 +0200

    [UPDATES] includes props for the clients informations

--- src/components/booking/BookingRecap.jsx
+++ src/components/booking/BookingRecap.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-const BookingRecap = () => {
-  // const [destination, setDestination] = useState("");
-  // const [ship, setShip] = useState("");
-
+const BookingRecap = ({
+  firstName,
+  lastName,
+  passengers,
+  destination,
+  startDate,
+  endDate,
+  // ships,
+  closeModalRecap,
+}) => {
   return (
+    // Modal decoration
     <div className="fixed top-[20%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[url(assets/images/modal_bg.jpg)] mt-[15%] bg-cover bg-center w-5/6 h-1/2 md:h-4/5 rounded-lg ">
       <div className="flex justify-between border-b-[2px]">
         <h1 className="pt-4 pl-6 text-4xl underline font-exo decoration-2-solid bp2_1:text-2xl">
@@ -13,20 +20,22 @@ const BookingRecap = () => {
         </h1>
         <button
           type="button"
+          onClick={closeModalRecap}
           className="self-end mr-4 text-5xl text-slate-50 text-shadow-3 font-orb -mt-52 hover:scale-105 bp2_1:text-2xl"
         >
           X
         </button>
       </div>
-      {/* Client informations */}
+      {/* CLIENT INFORMATIONS */}
       <div className="pt-4 pb-2 pl-6 text-2xl font-exo bp2_1:text-lg">
-        Nom : PLUVINAGE <br />
-        Nombre de personnes : {Math.floor(Math.random() * 10)} <br />
-        Destination : YAVIN 4<br />
-        Départ : {Math.floor(Math.random() * 20)} BBY <br />
-        Durée du séjour : {Math.floor(Math.random() * 20)} jours <br />
-        Hébergement : Hotel de luxe <br />
-        Transport : Croiseur MC80 <br />
+        <p>Prénom : {firstName}</p>
+        <p>Nom : {lastName} </p>
+        <p>Nombre de personnes : {passengers} </p>
+        <p>Destination : {destination} </p>
+        <p>
+          Date : du {startDate} au {endDate}
+        </p>
+        {/* <p>Transport :</p> */}
       </div>
       <div className="flex justify-end pb-4 pr-6">
         <div className="rounded-lg justify-end font-exo bg-[url(assets/images/modal_dark_bg2.jpg)] bg-cover w-1/2 h-20 text-slate-50 text-2xl pl-4 pt-1 bp2_1:text-lg bp2_1:w-4/5">
@@ -38,7 +47,15 @@ const BookingRecap = () => {
   );
 };
 
-// BookingRecap.propTypes = {
-//   closeModal: PropTypes.func.isRequired,
-// };
+BookingRecap.propTypes = {
+  // modalRecap: PropTypes.func.isRequired,
+  closeModalRecap: PropTypes.func.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  passengers: PropTypes.number.isRequired,
+  destination: PropTypes.string.isRequired,
+  startDate: PropTypes.number.isRequired,
+  endDate: PropTypes.number.isRequired,
+  // ships: PropTypes.string.isRequired,
+};
 export default BookingRecap;
