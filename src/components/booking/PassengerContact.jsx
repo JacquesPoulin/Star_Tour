5669126e8f715826a502ed59fd5a001681a75d04
JacquesPoulin <jacques.poulin64@gmail.com>
2022-04-29 19:14:47 +0200

    [UPDATES] : Style updates

--- src/components/booking/PassengerContact.jsx
+++ src/components/booking/PassengerContact.jsx
@@ -3,20 +3,20 @@ import PropTypes from "prop-types";
 
 const PassengerContact = ({ phone, setPhone, email, setEmail }) => {
   return (
-    <div>
-      <label htmlFor="phone" className=" w-32 mt-6 tracking-wide">
+    <div className="mt-9  w-full flex justify-evenly">
+      <label htmlFor="phone" className=" m-auto w-auto tracking-wide text-xl">
         Téléphone *
         <input
           id="phone"
-          type="phone"
+          type="text"
           value={phone}
           onChange={(e) => setPhone(e.target.value)}
           required
-          className="w-auto mt-4 rounded-sm focus:border-lime-500 text-slate-900"
+          className="cursor-pointer w-auto pl-3 ml-2 text-xl font-exo text-slate-50 dark:text-slate-900 bg-transparent border-0 border-b-4 border-l-4 border-slate-50 appearance-none focus:outline-none focus:ring-0 focus:border-[#7ad563] bp2_1:w-full tracking-wide"
         />
       </label>
 
-      <label htmlFor="mail" className="mt-4 tracking-wide">
+      <label htmlFor="mail" className="m-auto w-auto tracking-wide text-xl">
         Email *
         <input
           id="mail"
@@ -24,7 +24,7 @@ const PassengerContact = ({ phone, setPhone, email, setEmail }) => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
-          className="w-auto mt-4 rounded-sm focus:border-lime-500 text-slate-900"
+          className="cursor-pointer w-auto pl-3 ml-2 text-xl font-exo text-slate-50 dark:text-slate-900 bg-transparent border-0 border-b-4 border-l-4 border-slate-50 appearance-none focus:outline-none focus:ring-0 focus:border-[#7ad563] bp2_1:w-full tracking-wide"
         />
       </label>
     </div>
--- src/components/booking/SubmitForm.jsx
+++ src/components/booking/SubmitForm.jsx
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 
 const SubmitForm = ({ isChecked, handleCheck, openModalRecap }) => {
   return (
-    <div>
+    <div className="w-full flex flex-col text-center m-auto">
       <label
         htmlFor="acceptedTerms"
-        className="mt-20 tracking-wide text-center"
+        className="mt-20 text-xl tracking-wider text-center"
       >
         <input
           id="acceptedTerms"
@@ -15,7 +15,7 @@ const SubmitForm = ({ isChecked, handleCheck, openModalRecap }) => {
           type="checkbox"
           checked={isChecked}
           onClick={handleCheck}
-          className="mr-4 rounded-sm focus:border-lime-500"
+          className="ml-4 mr-4 rounded-sm focus:border-lime-500"
           required
         />
         J'accepte les termes du contrat *
@@ -23,7 +23,7 @@ const SubmitForm = ({ isChecked, handleCheck, openModalRecap }) => {
 
       <button
         type="button"
-        className="m-auto mt-20 mb-5 tracking-wide transition-all duration-700 bg-gray-800 rounded-sm cursor-pointer w-36 hover:scale-110"
+        className="text-center m-auto mt-14 text-2xl cursor-pointer w-36 hover:scale-110 tracking-wider animate-pulse"
         onClick={openModalRecap}
       >
         Submit
